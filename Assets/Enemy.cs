74048cb9d5a0b1719fb3aff0f234c2c271f43925
Niko Pinnis <niko.pinnis@gmail.com>
2019-08-08 20:32:39 +0300

    After L105

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+
+    private void Start()
+    {
+        gameObject.AddComponent<BoxCollider>();
+        gameObject.GetComponent<BoxCollider>().isTrigger = false;
+    }
+
     private void OnParticleCollision(GameObject other)
     {
         //print($"particles collided with {other.name}");
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
---
+++ Assets/ScoreBoard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreBoard : MonoBehaviour
+{
+    int score;
+    Text scoreText;
+
+    [SerializeField] int scorePerHit = 12;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        scoreText.text = $"{score}";
+    }
+
+    public void ScoreHit()
+    {
+        score += scorePerHit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
